848823314dd1e8eaa3e51b23270bd3e303f61b4f
Linda Allen <linda.allen@acurian.com>
2018-07-22 17:26:53 -0400

    add selected class to easy/hard button, generate new colors and num of squares for easy/hard mode, and play again mode tracking

--- colorGame.js
+++ colorGame.js
@@ -1,25 +1,57 @@
-var colors = generateRandomColors(6);
-
+var numSquares = 6;
+var colors = generateRandomColors(numSquares);
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
 
-resetButton.addEventListener("click", function(){
+
+easyBtn.addEventListener("click", function() {
+	easyBtn.classList.add("selected");
+	hardBtn.classList.remove("selected");
+	numSquares = 3;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++) {
+		if(colors[i]) {
+			squares[i].style.background = colors[i];
+		} else {
+			squares[i].style.display = "none";
+		}
+	}
+});
+
+hardBtn.addEventListener("click", function() {
+	hardBtn.classList.add("selected");
+	easyBtn.classList.remove("selected");
+	numSquares = 6;
+	colors = generateRandomColors(numSquares);
+	pickedColor = pickColor();
+	colorDisplay.textContent = pickedColor;
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.background = colors[i];
+		squares[i].style.display = "block";
+	}
+});
+
+resetButton.addEventListener("click", function() {
 	//generate all new colors
-	colors = generateRandomColors(6);
+	colors = generateRandomColors(numSquares);
 	//pick a new random color from array
 	pickedColor = pickColor();
 	//change colorDisplay to match picked Color
 	colorDisplay.textContent = pickedColor;
-	//change color of squares
-	for(var i = 0; i <squares.length; i++){
+	//change colors of squares
+	for(var i = 0; i <squares.length; i++) {
 		squares[i].style.background = colors[i];
 	}
 	h1.style.background = "#232323";
-})
+});
 
 colorDisplay.textContent = pickedColor;
 
@@ -56,11 +88,11 @@ function pickColor() {
 	return colors[random];
 }
 
-function generateRandomColors(num){
+function generateRandomColors(num) {
 	//make an array
 	var arr = [];
 	//repeat num times
-	for(var i = 0; i < num; i++){
+	for(var i = 0; i < num; i++) {
 		//get random color and push into array
 		arr.push(randomColor());
 	}
